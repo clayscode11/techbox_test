d107264d349d6366ca294ab045ae3d9670f5aa2f
Clayton <bartlettclayton0@gmail.com>
2025-09-21 21:47:07 -0700

    UI Skeleton built

---
+++ index.js
@@ -0,0 +1,10 @@
+const createEl = document.getElementById("button-el");
+const dialog = document.getElementById("card-dialog");
+
+function openModal() {
+  dialog.showModal();
+}
+
+function closeModal() {
+  dialog.close();
+}
